3fbe8581f64a8ded09da7f957485b98d53719252
ChamdeepM <chamdeep.mudigolam@thomsonreuters.com>
2022-06-29 10:22:32 +0000

    Date format change for endusers

--- app/resources/controller/OTP.controller.js
+++ app/resources/controller/OTP.controller.js
@@ -271,6 +271,31 @@ function(Controller, MessageBox, Token, BusyIndicator, formatter, Dialog, Messag
             }
             this.getView().byId("fromDate").setDateValue(new Date(date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + firstDay));
             this.getView().byId("toDate").setDateValue(new Date(date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + lastDay));
+            ///----------- Set DateFormat for EndUsers -----------------//////
+            var userLang = navigator.language || navigator.userLanguage;
+            const sample = window.Intl ? new Intl.DateTimeFormat(userLang, {
+                numberingSystem: 'latn',
+                calendar: 'gregory'
+            }).format(new Date(1970, 11, 31)) : '';
+            let mm = 0,
+                mi = sample.indexOf(12);
+            let dd = 1,
+                di = sample.indexOf(31);
+            let yy = 2,
+                yi = sample.indexOf(1970);
+            // IE 10 or earlier, iOS 9 or earlier, non-Latin numbering system
+            // or non-Gregorian calendar; fall back to mm/dd/yyyy
+            if (yi >= 0 && mi >= 0 && di >= 0) {
+                mm = (mi > yi) + (mi > di);
+                dd = (di > yi) + (di > mi);
+                yy = (yi > mi) + (yi > di);
+            }
+            let r = [];
+            r[yy] = 'yyyy';
+            r[mm] = 'MM';
+            r[dd] = 'dd';
+            this.getView().byId("fromDate").setDisplayFormat(r.join(sample.match(/[-.]/) || '/'));
+            this.getView().byId("toDate").setDisplayFormat(r.join(sample.match(/[-.]/) || '/'));
         },
         _getCsrfToken: function(Resolve, Reject) {
             //////------- get x csrf token for post call ------//////
